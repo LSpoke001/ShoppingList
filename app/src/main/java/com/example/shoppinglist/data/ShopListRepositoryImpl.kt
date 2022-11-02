cbfd97fe22e239a0beaf5d28b3fc7371e26f3a11
Elena Spytsyna <lspoke.larn@gmail.com>
2022-11-02 12:03:02 +0300

    added impl repository

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,43 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if(shopItem.id == ShopItem.DEFAULT_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+        autoIncrementId--
+    }
+
+    override fun deleteShopItemById(id: Int) {
+        shopList.removeAt(id)
+        autoIncrementId--
+    }
+
+    override fun getShopItemById(id: Int): ShopItem {
+        return shopList.find {
+            it.id == id } ?: throw RuntimeException(
+            "Element with id $id not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun updateShopList(shopItem: ShopItem) {
+        val oldElement = getShopItemById(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+}
